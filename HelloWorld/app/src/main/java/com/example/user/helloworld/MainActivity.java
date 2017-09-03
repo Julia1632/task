eff7c0ffd0707f5145962bd2171553e6c9d56add
Julia1632 <julia.zhurun6552817w@gmail.com>
2017-09-03 20:14:30 +0300

    create project

---
+++ HelloWorld/app/src/main/java/com/example/user/helloworld/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.user.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button =(Button) findViewById(R.id.button);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this,"Hello EPAM!", Toast.LENGTH_LONG);
+                toast.show();
+
+            }
+        });
+
+}
+    }
